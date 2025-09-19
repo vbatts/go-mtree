vbatts/tar-split

commit 21723a3974bc47e3950708caf5cfdf4a65086efc

*: fix comparison of missing keywords
Adding another test validated from the FreeBSD workflow.

Just because the keywords requested to be validated are not present in
the manifest, it is not an error.
Also, if the keywords from a new manifest are not present in a prior
manifest, then only compare the common keywords.

Fixes https://github.com/vbatts/go-mtree/issues/86

Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

compare.go
@@ -185,10 +185,16 @@ func compareEntry(oldEntry, newEntry Entry) ([]KeyDelta, error) {
 	}
 
 	diffs := map[Keyword]*stateT{}
+	oldKeys := oldEntry.AllKeys()
+	newKeys := newEntry.AllKeys()
 
 	// Fill the map with the old keys first.
-	for _, kv := range oldEntry.AllKeys() {
+	for _, kv := range oldKeys {
 		key := kv.Keyword()
+		// only add this diff if the new keys has this keyword
+		if key != "tar_time" && key != "time" && HasKeyword(newKeys, key) == emptyKV {
+			continue
+		}
 
 		// Cannot take &kv because it's the iterator.
 		copy := new(KeyVal)
@@ -202,8 +208,12 @@ func compareEntry(oldEntry, newEntry Entry) ([]KeyDelta, error) {
 	}
 
 	// Then fill the new keys.
-	for _, kv := range newEntry.AllKeys() {
+	for _, kv := range newKeys {
 		key := kv.Keyword()
+		// only add this diff if the old keys has this keyword
+		if key != "tar_time" && key != "time" && HasKeyword(oldKeys, key) == emptyKV {
+			continue
+		}
 
 		// Cannot take &kv because it's the iterator.
 		copy := new(KeyVal)
@@ -319,12 +329,6 @@ func Compare(oldDh, newDh *DirectoryHierarchy, keys []Keyword) ([]InodeDelta, er
 		New *Entry
 	}
 
-	// Make dealing with the keys mapping easier.
-	keySet := map[Keyword]struct{}{}
-	for _, key := range keys {
-		keySet[key] = struct{}{}
-	}
-
 	// To deal with different orderings of the entries, use a path-keyed
 	// map to make sure we don't start comparing unrelated entries.
 	diffs := map[string]*stateT{}
@@ -405,7 +409,7 @@ func Compare(oldDh, newDh *DirectoryHierarchy, keys []Keyword) ([]InodeDelta, er
 			if keys != nil {
 				var filterChanged []KeyDelta
 				for _, keyDiff := range changed {
-					if _, ok := keySet[keyDiff.name]; ok {
+					if InKeywordSlice(keyDiff.name, keys) {
 						filterChanged = append(filterChanged, keyDiff)
 					}
 				}

check_test.go
@@ -69,7 +69,7 @@ func TestCheckKeywords(t *testing.T) {
 		t.Fatal(err)
 	}
 	if len(res) != 1 {
-		t.Errorf("expected to get 1 delta on changed mtimes, but did not")
+		t.Fatal("expected to get 1 delta on changed mtimes, but did not")
 	}
 	if res[0].Type() != Modified {
 		t.Errorf("expected to get modified delta on changed mtimes, but did not")

cmd/gomtree/main.go
@@ -184,10 +184,6 @@ func app() error {
 			if (keyword == "time" && mtree.InKeywordSlice("tar_time", specKeywords)) || (keyword == "tar_time" && mtree.InKeywordSlice("time", specKeywords)) {
 				continue
 			}
-
-			if !mtree.InKeywordSlice(keyword, specKeywords) {
-				return fmt.Errorf("cannot verify keywords not in mtree specification: %s\n", keyword)
-			}
 		}
 	}
 
@@ -263,9 +259,9 @@ func app() error {
 			if isTarSpec(specDh) || *flTar != "" {
 				res = filterMissingKeywords(res)
 			}
-			if len(res) > 0 {
-				return fmt.Errorf("unexpected missing keywords: %d", len(res))
-			}
+			//if len(res) > 0 {
+			//return fmt.Errorf("unexpected missing keywords: %d", len(res))
+			//}
 
 			out := formatFunc(res)
 			if _, err := os.Stdout.Write([]byte(out)); err != nil {

test/cli/0005.sh
@@ -1,0 +1,19 @@
+#!/bin/bash
+set -e
+
+name=$(basename $0)
+root=$1
+gomtree=$(readlink -f ${root}/gomtree)
+t=$(mktemp -d /tmp/go-mtree.XXXXXX)
+
+echo "[${name}] Running in ${t}"
+
+pushd ${root}
+mkdir -p ${t}/extract
+git archive --format=tar HEAD^{tree} . | tar -C ${t}/extract/ -x
+
+${gomtree} -k sha1digest -c -p ${t}/extract/ > ${t}/${name}.mtree
+${gomtree} -f ${t}/${name}.mtree -k md5digest -p ${t}/extract/
+
+popd
+rm -rf ${t}

vbatts/go-mtree

commit b053cf7fda9b3d1d6c585b77c78368c64decd7c6

compare: move FreeBSD loose keyword comparisons to gomtree command
FreeBSD has quite unfortunate behaviour when dealing with keywords that
are missing in one of the manifests being compared -- namely, they
ignore these instances.

Commit 21723a3974bc ("*: fix comparison of missing keywords") re-added
this behaviour after the introduction of the Compare API, but
unfortunately it was implemented in the Compare API itself -- meaning
that library users (which didn't want this behaviour) were silently
opted into it.

This patch moves the behaviour to the command-line, where it belongs
(a future patch in this series will allow users to opt-out of this
unfortunate behaviour, as well as some other unfortunate FreeBSD
compatibility behaviours).

Fixes: 21723a3974bc ("*: fix comparison of missing keywords")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

cmd/gomtree/cmd/validate.go
@@ -376,6 +376,7 @@ func validateAction(c *cli.Context) error {
 		if isTarSpec(specDh) || c.String("tar") != "" {
 			filters = append(filters, tarKeywordFilter)
 		}
+		filters = append(filters, freebsdCompatKeywordFilter)
 		res = filterDeltas(res, filters...)
 
 		if len(res) > 0 {
@@ -463,6 +464,25 @@ func tarKeywordFilter(delta *mtree.InodeDelta) bool {
 			return kd.Name() == "size"
 		})
 	}
+	return true
+}
+
+// freebsdCompatKeywordFilter removes any deltas where a key is not present in
+// both manifests being compared. This is necessary for compatibility with
+// FreeBSD's mtree(8) but is generally undesireable for most users.
+func freebsdCompatKeywordFilter(delta *mtree.InodeDelta) bool {
+	if delta.Type() != mtree.Modified {
+		return true
+	}
+	keys := delta.DiffPtr()
+	*keys = slices.DeleteFunc(*keys, func(kd mtree.KeyDelta) bool {
+		if kd.Name().Prefix() == "xattr" {
+			// Even in FreeBSD compatibility mode, any xattr changes should
+			// still be treated as a proper change and not filtered out.
+			return false
+		}
+		return kd.Type() != mtree.Modified
+	})
 	return true
 }
 

compare.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"iter"
-	"maps"
 	"slices"
 
 	"github.com/sirupsen/logrus"
@@ -246,31 +245,6 @@ func compareEntry(oldEntry, newEntry Entry) ([]KeyDelta, error) {
 		oldKeys = oldEntry.allKeysMap()
 		newKeys = newEntry.allKeysMap()
 	)
-
-	// Delete any keys which are not present in both maps.
-	keyFilterFn := func(otherMap map[Keyword]KeyVal) func(Keyword, KeyVal) bool {
-		return func(k Keyword, _ KeyVal) bool {
-			switch {
-			case k.Prefix() == "xattr":
-				// xattrs are presented as one keyword to users but are actually
-				// implemented as separate keywords and so we should always include
-				// them (even if the same xattr is not present in both sides).
-				// TODO: I actually think this is not inline with the original
-				//       purpose of this code, but I'm leaving it as-is to not not
-				//       introduce bugs.
-				return false
-			case k == "time" || k == "tar_time":
-				// These have special handling later.
-				return false
-			default:
-				// Drop keys which do not exist in the other entry.
-				_, ok := otherMap[k]
-				return !ok
-			}
-		}
-	}
-	maps.DeleteFunc(oldKeys, keyFilterFn(newKeys))
-	maps.DeleteFunc(newKeys, keyFilterFn(oldKeys))
 
 	// If both tar_time and time were specified in the set of keys, we have to
 	// convert the "time" entries to "tar_time" to allow for tar archive

compare_test.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"slices"
 	"testing"
 	"time"
 
@@ -228,6 +229,77 @@ func TestCompareKeySubset(t *testing.T) {
 	if !assert.Empty(t, diffs, "size-only compare should not return any entries") {
 		pprintInodeDeltas(t, diffs)
 	}
+}
+
+func TestCompareKeyDelta(t *testing.T) {
+	dir := t.TempDir()
+
+	// Create a bunch of objects.
+	tmpfile := filepath.Join(dir, "tmpfile")
+	require.NoError(t, os.WriteFile(tmpfile, []byte("some content here"), 0666))
+
+	tmpdir := filepath.Join(dir, "testdir")
+	require.NoError(t, os.Mkdir(tmpdir, 0755))
+
+	tmpsubfile := filepath.Join(tmpdir, "anotherfile")
+	require.NoError(t, os.WriteFile(tmpsubfile, []byte("aaa"), 0666))
+
+	// Walk the current state.
+	manifestKeywords := append(DefaultKeywords[:], "sha1digest")
+	old, err := Walk(dir, nil, manifestKeywords, nil)
+	require.NoErrorf(t, err, "walk %s", dir)
+
+	t.Run("Extra-Key", func(t *testing.T) {
+		extraKeyword := Keyword("sha256digest")
+		newManifestKeywords := append(manifestKeywords[:], extraKeyword)
+
+		new, err := Walk(dir, nil, newManifestKeywords, nil)
+		require.NoErrorf(t, err, "walk %s", dir)
+
+		diffs, err := Compare(old, new, nil)
+		require.NoError(t, err, "compare")
+
+		assert.NotEmpty(t, diffs, "extra keys in manifest should result in deltas")
+		for _, diff := range diffs {
+			if assert.Equal(t, Modified, diff.Type(), "extra keyword diff element should be 'modified'") {
+				kds := diff.Diff()
+				if assert.Len(t, kds, 1, "should only get a single key delta") {
+					kd := kds[0]
+					assert.Equalf(t, Extra, kd.Type(), "key %q", kd.Name())
+					assert.Equal(t, extraKeyword, kd.Name())
+					assert.Nil(t, kd.Old(), "Old for extra keyword delta")
+					assert.NotNil(t, kd.New(), "New for extra keyword delta")
+				}
+			}
+		}
+	})
+
+	t.Run("Missing-Key", func(t *testing.T) {
+		missingKeyword := Keyword("sha1digest")
+		newManifestKeywords := slices.DeleteFunc(manifestKeywords[:], func(kw Keyword) bool {
+			return kw == missingKeyword
+		})
+
+		new, err := Walk(dir, nil, newManifestKeywords, nil)
+		require.NoErrorf(t, err, "walk %s", dir)
+
+		diffs, err := Compare(old, new, nil)
+		require.NoError(t, err, "compare")
+
+		assert.NotEmpty(t, diffs, "missing keys in manifest should result in deltas")
+		for _, diff := range diffs {
+			if assert.Equal(t, Modified, diff.Type(), "missing keyword diff element should be 'modified'") {
+				kds := diff.Diff()
+				if assert.Len(t, kds, 1, "should only get a single key delta") {
+					kd := kds[0]
+					assert.Equalf(t, Missing, kd.Type(), "key %q", kd.Name())
+					assert.Equal(t, missingKeyword, kd.Name())
+					assert.NotNil(t, kd.Old(), "Old for missing keyword delta")
+					assert.Nil(t, kd.New(), "New for missing keyword delta")
+				}
+			}
+		}
+	})
 }
 
 //gocyclo:ignore