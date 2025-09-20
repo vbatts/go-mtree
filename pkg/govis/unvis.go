3fa4a0aeddb5ae899cc3c97f150a05526943225f
Aleksa Sarai <cyphar@cyphar.com>
2025-09-20 16:32:42 +1000

    govis: add SPDX license tag
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- pkg/govis/unvis.go
+++ pkg/govis/unvis.go
@@ -1,6 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
 /*
  * govis: unicode aware vis(3) encoding implementation
- * Copyright (C) 2017 SUSE LLC.
+ * Copyright (C) 2017-2025 SUSE LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
